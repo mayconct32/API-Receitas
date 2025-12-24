1bf468135eb89cf6c9f511d74a3b34b8d1fe0a2e
mayconcorreatinti <maycon326754@gmail.com>
2025-12-24 11:57:15 -0300

    feat(users): adiciona endpoint add_user para a rota /users POST

--- src/api/routers/users.py
+++ src/api/routers/users.py
@@ -1,13 +1,34 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from http import HTTPStatus
 
 
 app = APIRouter(tags=["users"],prefix="/users")
 
-@app.get("/")
-def get_users():
-    users = [
+#schema
+class User(BaseModel):
+    username: str
+    email: str
+
+#fake db
+users = [
         {"username":"user1","email":"user1@example.com"},
         {"username":"user2","email":"user2@example.com"},
         {"username":"user3","email":"user3@example.com"}
     ]
+
+@app.get("/",status_code = HTTPStatus.OK)
+def get_users():
     return users
+
+@app.post("/",status_code = HTTPStatus.CREATED)
+def add_user(user:User):
+    users.append(
+        {
+            "username":user.username,
+            "email":user.email
+        }
+    )
+
+    return {"message":"User added successfully."}
+

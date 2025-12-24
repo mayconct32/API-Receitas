80feb302c4473ed5021db9a0246988a01d0f67b7
mayconcorreatinti <maycon326754@gmail.com>
2025-12-24 11:16:23 -0300

    feat(include_router): insere operaçõens da rota users na main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import uvicorn 
+from api.routers.users import app as users
+
 
 
 app = FastAPI()
@@ -7,6 +9,8 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message":"hello world!"}
+
+app.include_router(users)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

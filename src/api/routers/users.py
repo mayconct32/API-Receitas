9b38a0e2ec0c50db54b9e875d03b2d64a91df628
mayconcorreatinti <maycon326754@gmail.com>
2025-12-24 23:16:33 -0300

    remove(obsolete code): remove codigo obsoleto dos endpoints da rota users

--- src/api/routers/users.py
+++ src/api/routers/users.py
@@ -5,40 +5,19 @@ from http import HTTPStatus
 
 app = APIRouter(tags=["users"],prefix="/users")
 
-#schema
-class User(BaseModel):
-    username: str
-    email: str
-
-#fake db
-users = []
-
 @app.get("/",status_code = HTTPStatus.OK)
 def get_users():
-    return users
+    ...
 
 @app.post("/",status_code = HTTPStatus.CREATED)
-def add_user(user:User):
-    users.append(user)
-    return {"message":"User added successfully."}
+def add_user(user):
+    ...
 
 @app.delete("/",status_code = HTTPStatus.OK)
 def delete_user(posiçao:int):
-    if posiçao>len(users) - 1:
-        raise HTTPException(
-            detail = "usuario não encontrado!",
-            status_code = HTTPStatus.NOT_FOUND
-        )
-    users.pop(posiçao)
-    return {"message":"User deleted successfully."}
+    ...
 
 @app.put("/",status_code = HTTPStatus.OK)
-def update_user(posiçao:int,user:User):
-    if posiçao>len(users) - 1:
-        raise HTTPException(
-            detail = "usuario não encontrado!",
-            status_code = HTTPStatus.NOT_FOUND
-        )
-    users[posiçao] = user
-    return user 
+def update_user(posiçao:int,user):
+    ...
 

1d30875b80c0e9f32defe6f3e274d78486c15893
mayconcorreatinti <maycon326754@gmail.com>
2025-12-31 20:17:40 -0300

    feat(repository) adiciona a class ChefRepository para interagir/acessar a tabela de Chefs do MySQL DB

---
+++ src/repositories/chef_repository.py
@@ -0,0 +1,22 @@
+from src.database import MysqlConnection
+from src.interfaces.repository import IRepository
+from typing import List
+
+
+class ChefRepository(IRepository[dict]):
+
+    def __init__(self) -> None:
+        self.connection = MysqlConnection() 
+
+    async def get_all(self) -> List[dict]:
+        chefs = await self.connection._query(
+        """
+            SELECT id,
+                chef_name,
+                email
+            FROM chef
+            LIMIT 20;
+        """
+        )
+        return chefs
+            

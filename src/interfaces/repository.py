4e57846e3ded03048aee8648d2c48a0ddf0c294a
mayconcorreatinti <maycon326754@gmail.com>
2025-12-30 23:59:20 -0300

    feat: adiciona a interface IRepository

---
+++ src/interfaces/repository.py
@@ -0,0 +1,25 @@
+from abc import ABC,abstractmethod
+from typing import List
+
+
+class IRepository[T](ABC):
+
+    @abstractmethod
+    def get_all(self) -> List[T]:
+       raise NotImplementedError
+
+    @abstractmethod
+    def get(self, id: int) -> T:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def add(self, **kwargs: object) -> None:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def delete(self, id: int) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self, id: int, **kwargs: object) -> None:
+        raise NotImplementedError

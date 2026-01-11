e0c24030ee6c8222d8aad0035c73289e5685744c
mayconct32 <maycon326754@gmail.com>
2026-01-10 21:57:28 -0300

    adiciona função para obter dependência ChefService

--- src/api/routers/dependencies.py
+++ src/api/routers/dependencies.py
@@ -1,6 +1,11 @@
 from src.services.chef_service import ChefService
 from src.repositories.chef_repository import ChefRepository
+from src.database import MysqlConnectionDB
 
 
 def get_chef_service() -> ChefService:
-    return ChefService(ChefRepository)
+    return ChefService(
+        ChefRepository(
+            MysqlConnectionDB()
+        )
+    )

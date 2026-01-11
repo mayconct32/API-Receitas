15cd7c375c3905e2ac5827d8507dc54aec7fc157
mayconct32 <maycon326754@gmail.com>
2026-01-11 18:40:12 -0300

    altera tabelas do banco de dados com implementação de CURRENT_TIMESTAMP para valor padrão em diversas colunas

--- src/tables.sql
+++ src/tables.sql
@@ -3,18 +3,18 @@ CREATE TABLE chef (
     chef_name VARCHAR(100) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash CHAR(97) NOT NULL,
-    create_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL
+    create_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE recipe (
     recipe_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    chef_id INTEGER NOT NULL,''
+    chef_id INTEGER NOT NULL,
     recipe_name VARCHAR(100) NOT NULL,
     description TEXT,
     prep_time time,
-    posted_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
+    posted_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     image_url CHAR(130),
     FOREIGN KEY(chef_id)
     REFERENCES chef(chef_id)

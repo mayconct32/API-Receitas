653e1f3c3650979e48a2900744bc31ebe815fba2
mayconct32 <maycon326754@gmail.com>
2026-01-10 21:59:36 -0300

    implementa método para obter todos os chefs de cozinha na classe ChefService

--- src/services/chef_service.py
+++ src/services/chef_service.py
@@ -6,4 +6,6 @@ class ChefService:
     def __init__(self,chef_repository:IRepository) -> None:
         self.chef_repository = chef_repository
     
-    
+    async def get_all_the_chefs(self):
+        chefs = await self.chef_repository.get_all()
+        return chefs

039bf865d4079219b21fa82ced3ad6a3c6264c34
mayconct32 <maycon326754@gmail.com>
2026-01-18 09:17:04 -0300

    feat: adiciona método para pegar o current user

--- src/services/chef_service.py
+++ src/services/chef_service.py
@@ -1,27 +1,31 @@
-import os
-from datetime import datetime, timedelta, timezone
 from http import HTTPStatus
-
-from fastapi import HTTPException
-from fastapi.security import (
-    OAuth2PasswordBearer,
-    OAuth2PasswordRequestForm,
-)
-from jwt import encode
+from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
+from fastapi import HTTPException,Depends
 from pwdlib import PasswordHash
-
+from datetime import datetime, timedelta, timezone
 from src.interfaces.repository import IRepository
 from src.models.chef import Chef, ResponseChef
+import os
+from jwt import encode,decode,InvalidTokenError
 
 
 class ChefSecurityService:
     def __init__(self, chef_repository: IRepository) -> None:
         self.chef_repository = chef_repository
         self.password_hash = PasswordHash.recommended()
-        self.oauth2_scheme = OAuth2PasswordBearer(tokenUrl="chefs/auth")
 
     def hash(self, password: str) -> str:
         return self.password_hash.hash(password)
+    
+    async def check_authentication(self, form_data: OAuth2PasswordRequestForm):
+        chef = await self.chef_repository.get(email=form_data.username)
+        if not chef or not self.verify_password(
+            form_data.password, chef["password_hash"]
+        ):
+            raise HTTPException(
+                detail="Incorrect username or password!",
+                status_code=HTTPStatus.FORBIDDEN,
+            )
 
     def verify_password(self, password, hash) -> bool:
         return self.password_hash.verify(password, hash)
@@ -48,17 +52,7 @@ class ChefSecurityService:
                 status_code=HTTPStatus.UNAUTHORIZED,
                 detail="unauthorized request",
             )
-
-    async def check_authentication(self, form_data: OAuth2PasswordRequestForm):
-        chef = await self.chef_repository.get(email=form_data.username)
-        if not chef or not self.verify_password(
-            form_data.password, chef["password_hash"]
-        ):
-            raise HTTPException(
-                detail="Incorrect username or password!",
-                status_code=HTTPStatus.FORBIDDEN,
-            )
-
+        
     async def create_access_token(self, form_data: OAuth2PasswordRequestForm):
         await self.check_authentication(form_data)
         to_encode = {"sub": form_data.username}
@@ -72,16 +66,37 @@ class ChefSecurityService:
             algorithm=os.getenv("ALGORITHM"),
         )
         return encoded_jwt
+    
 
+    async def get_current_user(self, token: str) -> dict:
+        credentials_exception = HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        try:
+            payload:dict = decode(
+                token,
+                os.getenv("SECRET_KEY"),
+                algorithms=[os.getenv("ALGORITHM")])
+            email = payload.get("sub")
+            if email is None:
+                raise credentials_exception
+        except InvalidTokenError:
+            raise credentials_exception
+        chef = await self.chef_repository.get(email=email)
+        if chef is None:
+            raise credentials_exception
+        return chef
+        
 
 class ChefService:
     def __init__(
         self,
         chef_repository: IRepository,
-        chef_sec_service: ChefSecurityService,
     ) -> None:
         self.chef_repository = chef_repository
-        self.chef_sec_service = chef_sec_service
+        self.chef_sec_service = ChefSecurityService(chef_repository)
 
     async def get_all_the_chefs(self, offset, limit):
         chefs = await self.chef_repository.get_all(offset, limit)
@@ -106,7 +121,7 @@ class ChefService:
         return chef
 
     async def delete_chef(self, chef_id, current_chef):
-        await self.chef_sec_service.check_authorization(
+        self.chef_sec_service.check_authorization(
             chef_id, current_chef["chef_id"]
         )
         await self.chef_repository.delete(current_chef["chef_id"])
@@ -115,7 +130,7 @@ class ChefService:
     async def update_chef(
         self, updated_chef: Chef, chef_id, current_chef
     ) -> ResponseChef:
-        await self.chef_sec_service.check_authorization(
+        self.chef_sec_service.check_authorization(
             chef_id, current_chef["chef_id"]
         )
         await self.chef_sec_service._verify_credentials(
@@ -124,8 +139,8 @@ class ChefService:
         await self.chef_repository.update((
             updated_chef.chef_name,
             updated_chef.email,
+            datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             self.chef_sec_service.hash(updated_chef.password),
-            datetime.now(),
             current_chef["chef_id"],
         ))
         updated_chef = await self.chef_repository.get(

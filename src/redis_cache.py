25a43f924bf45d8d99160352b06cdbc9f673e9db
mayconct32 <maycon326754@gmail.com>
2026-02-15 16:22:34 -0300

    adiciona interação assincrona com o redis

--- src/redis_cache.py
+++ src/redis_cache.py
@@ -1,4 +1,4 @@
-from redis import Redis
+from redis import asyncio
 import json
 from typing import List
 import os 
@@ -16,12 +16,12 @@ class RedisConnection:
         self.__connection = None
     
     def __connect(self) -> None:
-        self.__connection = Redis(
-            host=self.host,
-            port=self.port
+        self.__connection = asyncio.from_url(
+            f"redis://{self.host}:{self.port}",
+            decode_responses = True
         )
 
-    def get_connection(self) -> Redis:
+    def get_connection(self) -> asyncio.Redis:
         if not self.__connection:
             self.__connect()
         return self.__connection
@@ -31,20 +31,20 @@ class RedisRepository:
     def __init__(self, redis_conn: RedisConnection) -> None:
         self.redis_conn = redis_conn.get_connection()
 
-    def insert(self, key: str, value: any) -> None:
+    async def insert(self, key: str, value: any) -> None:
         if isinstance(value,list):
             #insert json
-            self.redis_conn.set(key,json.dumps(value))
+            await self.redis_conn.set(key,json.dumps(value))
         else:
-            self.redis_conn.set(key,value)
+            await self.redis_conn.set(key,value)
     
-    def get(self, key: str) -> List[dict] | str:
+    async def get(self, key: str) -> List[dict] | str:
         try:
-            return json.loads(self.redis_conn.get(key))
+            return json.loads(await self.redis_conn.get(key))
         except json.decoder.JSONDecodeError:
-            return self.redis_conn.get(key).decode("utf-8")
+            return await self.redis_conn.get(key)
     
-    def delete(self, key: str) -> None:
-        self.redis_conn.delete(key)
+    async def delete(self, key: str) -> None:
+        await self.redis_conn.delete(key)
 
 
